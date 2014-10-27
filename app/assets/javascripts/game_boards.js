45d540999a450b8fba27672d262c6ceaf2b2013b
steinfabian <fabian@fabianstein.com>
2014-10-28 09:11:51 +1100

    first version of about popup added

--- app/assets/javascripts/game_boards.js
+++ app/assets/javascripts/game_boards.js
@@ -7,6 +7,11 @@ $(document).ready(function () {
 	  midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
 	});
 
+  $('.about_popup_launch').magnificPopup({
+    type:'inline',
+    midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+  });
+
   // on click of the level button the currentLevel variable needs to be set and the launch level lightbox shall appear. How to write this in an efficient way to pass on the right level depending on which button the user clicked?
 	$(".level_welcome").on('click', function(event) {
 		event.preventDefault();
