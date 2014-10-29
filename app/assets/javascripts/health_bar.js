213ff5ed1f3d6898367781acead05fa797aacf9c
Andrew Thompson <andrew@imaginedbyandrew.com>
2014-10-29 17:08:34 +1100

    Added if statement to circle.add.entry function in health_bar.js file

--- app/assets/javascripts/health_bar.js
+++ app/assets/javascripts/health_bar.js
@@ -1,4 +1,5 @@
-var health = 0.8;
+var health = 1;
+var levelPassed = true;
 
 $(document).ready(function () {
 	var myCanvas = document.getElementById('my_canvas');
@@ -10,8 +11,13 @@ $(document).ready(function () {
   circle.addEntry({
       fillColor: 'rgba(65, 199, 36, 1)',
       progressListener: function() {
-          return health; // between 0 and 1
-      },
+        if (levelPassed === true){
+          return health - 0.1; // between 0 and 1
+        } else {
+          return health;
+        }
+      }
+
   }); 
   circle.start(33);
 });
