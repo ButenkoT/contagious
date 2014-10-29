5673c91cb5e340592637b5b7be8494dd0bce0841
ButenkoT <tanya@butenko.me>
2014-10-29 11:26:39 +1100

    update

--- app/assets/javascripts/game_boards.js
+++ app/assets/javascripts/game_boards.js
@@ -1,88 +1,95 @@
 
-var types = [1, 2, 3, 4, 5];
+var types = [1, 2, 3, 4, 5, 6];
 
+function generateItem() {
+  return types[Math.floor(Math.random() * types.length)];
+}
 
 function generate() {
-  $('.about_popup_launch').magnificPopup({
-    type:'inline',
-    midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
-  });
-
-  // on click of the level button the currentLevel variable needs to be set and the launch level lightbox shall appear. How to write this in an efficient way to pass on the right level depending on which button the user clicked?
-  $('.level_welcome').magnificPopup({
-  type:'inline',
-  midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
-  });
-
-	$(".level_welcome").on('click', function(event) {
-		event.preventDefault();
-		currentLevel = 1;
-		launchLevelLightbox();
-	});
-
-  var type = [1, 2, 3, 4, 5];
 
   //give us 100 values step by step randomisizng our type array values
-  return _.range(0, 100, 1).map(function(){
-    return types[Math.floor(Math.random() * 5)];
-  });
+  return _.range(0, 100, 1).map(generateItem);
 
 }
 
 
-function createBoard($board) {
-
+function fillBoard($board) {
   var template = Handlebars.compile($('#gameTemplate').html());
   $board.empty();
-
   //with handlebar append board-cells with value in the game-board
   _(generate()).each(function(type) {
     var $cell = $(template({type: type}))
       .appendTo($board);
   });
+  setCoordinates($board);  
 
-  setCoordinates($board);
+
+  var currentScore = $board.data('score') || 0;  
+  destroyMatches($board);
+  $board.data('score', currentScore);
 }
 
+//creating a board with handlebar template and filling it with cells with particular value (data-type from 1 to 5)
+function createBoard($board) {
+  fillBoard($board);
+  $board.data('score', 0);
+}
+
+function shuffle($board) {
+  fillBoard($board);
+
+  var currentScore = $board.data('score') || 0;
+  currentScore = currentScore - 100;
+  if (currentScore < 0) {
+    currentScore = 0;
+  }
+  $board.data('score', currentScore);
+}
+
+
+//setting coordinates of cell and its number
 function setCoordinates($board) {
   $board.find('.board-cell').each(function(index, cell) {
     var $cell = $(cell),
-      number = getNumber($cell)
-      coord = getCoordinates(number);
+      number = getNumber($cell)//calls for function gerNumber and passing it cell from the DOM
+      coord = getCoordinates(number); //calls for function getCoordinates and passing the number of element
 
-    $cell.find('.cell-x').text(coord[0]);
-    $cell.find('.cell-y').text(coord[1]);
+    $cell.find('.cell-x').text(coord[0]);//coordinate x is a 1st el of array
+    $cell.find('.cell-y').text(coord[1]);//coordinate y is a 2nd el of array
   })
 }
 
 
+//calculating the number of our cell by counting the previous elements
 function getNumber($cell) {
   return $cell.prevAll('.board-cell').length;
 } 
+
 
 //getting x and y coordinates of a clicked board-cell  
 function getCoordinates(number) {    
   var x = number % 10;
   var y = Math.floor(number / 10);
-  return [x, y];
+  return [x, y]; //returns array of x and y coordinates so we can access the via position [0] and [1]
 }
 
 
-
+//swapping 2 cells by changing their data-type 
 function swap($new, $old) {
   var _type = $new.attr('data-type');
   $new.attr('data-type', $old.attr('data-type'));
   $old.attr('data-type', _type);
+  $('.board-cell').removeClass('clicked');
 }
 
 
-
-// $someCell = getCell($board, [1,1]);
+//finds the cell on particular position
 function getCell($board, coordinates) {
   return $board.find('.board-cell').eq(coordinates[0] + coordinates[1] * 10);
 }
 
 
+//check if 2nd clicked cell is in the range of 1 from the 1st clicked cell by vertical(y) and horizontal(x) coordinates
 function isNear($new, $old) {
   var newcoord = getCoordinates(getNumber($new));
   var oldcoord = getCoordinates(getNumber($old));
@@ -97,114 +104,135 @@ function isNear($new, $old) {
 };
 
 
+function destroyMatchesItem($board, cell, index) {
+  var $cell = $(cell),
+  coords = getCoordinates(index),
+  x = coords[0],
+  y = coords[1],
+  toDestroyRight = [],
+  toDestroyDown = [];
 
 
+  for( var i = 1; (i <= 4) && (x + i <= 9) &&
+    (toDestroyRight.length === i - 1) && $cell.attr('data-type') !== 0; i ++) {
+    var $cellRight = getCell($board, [x + i, y]);
+    if ($cellRight.attr('data-type') === $cell.attr('data-type')){
+      toDestroyRight.push($cellRight[0]);
+    }
 
+  }
 
+  for( var i = 1; (i <= 4) && (y + i <= 9) &&
+    (toDestroyDown.length === i - 1) && $cell.attr('data-type') !== 0; i ++) {
+    var $cellDown = getCell($board, [x, y + i]);
+    if ($cellDown.attr('data-type') === $cell.attr('data-type')) {
+      toDestroyDown.push($cellDown[0]);
+    }
+
+  }
+
+  if ( toDestroyRight.length >= 2) {
+    $(toDestroyRight.concat(cell)).attr('data-type', 0);
+  }
+
+  if ( toDestroyDown.length >= 2) {
+    $(toDestroyDown.concat(cell)).attr('data-type', 0);
+  }
+
+  return $board.find('.board-cell[data-type="0"]').length > 0;
+}
+
+//check on if any 3 matches present and turns data-type matches of 3 and more vertical or horizontal into 0 and calculate the score of destroyed elements
 function destroyMatches($board, isDestoyed) {
+  var $cells = $board.find('.board-cell'),
+  somethingDestroyed = false;
 
+  for(var i = 0; i < $cells.length && !somethingDestroyed; i = i + 1) {
+    somethingDestroyed = destroyMatchesItem($board, $cells[i], i);
+  }
+  
 
-  // do something
-/*
-
-$board.find('asdfasdf').eash()
-
-    Xoo
-    o
-    o
-
-*/
-  // 
-
-
-
-  if ($board.find('.board-cell[data-type="0"]').length > 0) {
-    $board.data('score', $board.data('score') + 12341234);
+  if (somethingDestroyed) {
+    $board.data('score', ($board.data('score') || 0) + 10);
 
     fillMissing($board);
 
+    console.log('recursive destroy');
     return destroyMatches($board, true);
   }
-
-
-//check on the board if any 3 or more inline vertical or horizontal (match_3)
-  // TODO: fill me
-  //
-
 
   return isDestoyed;
 }
 
+
+//taking all cells with data-type 0 and replace them with a new random data-type(1..5)
 function fillMissing($board) {
-  // TODO: fill me
-  return false;
+  $board.find('.board-cell[data-type="0"]').each(function(index, item) {
+    $(item).attr('data-type', generateItem());
+  });
+  return true;
 }
-
-
-//check if 2nd clicked cell is in a range of 1st clicked cell via coordinates
-
-
-
-  //swap clicked board-cell with 2nd clicked cell
-    //make check on the board(match_3)
-      //if any matches leave the move
-      // delete matches
-      //calculate sum
-
-
-      // else return clicked board-cell back on place
-//else unclick both  
-
-
-
 
 
 $(document).ready(function(){
 
-//each cell reacts on click 
-$(document.body)
-
-  .on('click', '.game-board .board-cell.clicked', function () {
-    $(this).removeClass('clicked');
-  })
-
-  .on('click', '.game-board .board-cell:not(.clicked)', function () {
-    var $cell = $(this),
-      $board = $cell.closest('.game-board'),
-      $prev;
-
-    // 1. If nothing clicked before
-    if (!$board.find('.board-cell.clicked').length) {
-      $cell.addClass('clicked');
-      return;
-    }
-
-    $prev = $board.find('.board-cell.clicked');
-
-    // 2. something illegal was clicked
-    if (!isNear($cell, $prev)) {
-      $prev.removeClass('clicked');
-      $cell.addClass('clicked');
-      return;
-    }
-      
-    // 3. something was clicked and it is legal
-    swap($cell, $prev);
-    if (destroyMatches($board, false)) {
-// $board.data('score')
-
-      fillMissing($board);
-    } else {
-      swap($cell, $prev);
-    }
-
-    $board.find('.board-cell.clicked').removeClass('clicked');
-
-
+  //reaction on about link
+  $('.about_popup_launch').magnificPopup({
+    type:'inline',
+    midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
   });
 
+  // on click of the level button the currentLevel variable needs to be set and the launch level lightbox shall appear. How to write this in an efficient way to pass on the right level depending on which button the user clicked?
+  $(".level_welcome").on('click', function(event) {
+    event.preventDefault();
+    currentLevel = 1;
+    launchLevelLightbox();
+  });
+ 
+  //each cell reacts on click 
+  $(document.body)
 
-  createBoard($('.game-board'));
+    .on('click', '.game-board .board-cell.clicked', function () {
+      $(this).removeClass('clicked');
+    })
+
+    .on('click', '.game-board .board-cell:not(.clicked)', function () {
+      var $cell = $(this),
+        $board = $cell.closest('.game-board'),
+        $prev;
+
+      // 1. If nothing clicked before
+      if (!$board.find('.board-cell.clicked').length) {
+        $cell.addClass('clicked');
+        return;
+      }
+
+      $prev = $board.find('.board-cell.clicked');
+
+      // 2. something illegal was clicked
+      if (!isNear($cell, $prev)) {
+        $prev.removeClass('clicked');
+        $cell.addClass('clicked');
+        return;
+      }
+        
+      // 3. something was clicked and it is legal
+      swap($cell, $prev);
+      if (destroyMatches($board, false)) {
+        // $board.data('score')
+          console.log('somethingDestroyed!!!!');
+          console.log('Current score:', $board.data('score'));
+        } else {
+          swap($cell, $prev);
+        }
+
+        $board.find('.board-cell.clicked').removeClass('clicked');
+
+
+    });
+
+
+    createBoard($('.game-board'));
 });
 
 
